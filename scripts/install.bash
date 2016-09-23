f2188db1bda845633afd4d6c61015d454ce6f0cc
FabMSTIC <fabmstic.lig@gmail.com>
2016-09-23 22:18:05 +0200

    correction install.sh

--- scripts/install.bash
+++ scripts/install.bash
@@ -7,10 +7,19 @@ fi
 
 export ROBAIR_HOME=`pwd`
 
-echo "" >> ~/.bashrc
-echo "#ROBAIR SETTINGS" >> ~/.bashrc
-echo "export ROBAIR_HOME=$ROBAIR_HOME" >> ~/.bashrc
-echo "source \$ROBAIR_HOME/scripts/env.bash"  >> ~/.bashrc
+
+read -r -p "Configurer ~/bashrc ? [O/n] " response
+case $response in
+	[nN]) 
+	;;
+	*)
+	echo "" >> ~/.bashrc
+	echo "#ROBAIR SETTINGS" >> ~/.bashrc
+	echo "export ROBAIR_HOME=$ROBAIR_HOME" >> ~/.bashrc
+	echo "source \$ROBAIR_HOME/scripts/env.bash"  >> ~/.bashrc
+	;;
+esac
+
 
 export ROBAIR_IP=`ip route get 8.8.8.8 | awk 'NR==1 {print $NF}'`
 export PATH="$PATH:$ROBAIR_HOME/scripts/"
@@ -26,17 +35,26 @@ echo "Veuillez entrer votre mot de passe pour installer les packages (sudo)"
 
 sudo -E apt-get update
 
+
+
+
 echo "$(tput setaf 1)Installation $(tput setab 7)coturn git make $(tput sgr0)"
 sudo -E apt-get install git make coturn
 
 
-echo "$(tput setaf 1)Installation $(tput setab 7)signalmaster$(tput sgr0)"
-git clone https://github.com/andyet/signalmaster.git
 
+read -r -p "Installation signalmaster ? [O/n] " response
+case $response in
+	[nN]) 
+	;;
+	*)
 
-cp $ROBAIR_HOME/configs/signalmaster.json $ROBAIR_HOME/signalmaster/config/development.json
-python $ROBAIR_HOME/scripts/editjson.py $ROBAIR_HOME/signalmaster/config/development.json server:key $ROBAIR_HOME/ssl/device.key
-python $ROBAIR_HOME/scripts/editjson.py $ROBAIR_HOME/signalmaster/config/development.json server:cert $ROBAIR_HOME/ssl/device.crt
+	git clone https://github.com/andyet/signalmaster.git
+	cp $ROBAIR_HOME/configs/signalmaster.json $ROBAIR_HOME/signalmaster/config/development.json
+	python $ROBAIR_HOME/scripts/editjson.py $ROBAIR_HOME/signalmaster/config/development.json server:key $ROBAIR_HOME/ssl/device.key
+	python $ROBAIR_HOME/scripts/editjson.py $ROBAIR_HOME/signalmaster/config/development.json server:cert $ROBAIR_HOME/ssl/device.crt
+	;;
+esac
 
 read -r -p "Voulez vous générer une autorité de certification ? [O/n] " response
 case $response in
@@ -51,7 +69,9 @@ esac
 
 read -r -p "Voulez vous générer un certificat ssl ? [O/n] " response
 case $response in
-	[oO]) 
+	[nN]) 
+	;;
+	*)
 	./scripts/createDeviceCRT.bash
 	;;
 esac
@@ -82,13 +102,23 @@ sudo apt-get install ros-kinetic-rosserial-arduino
 
 sudo apt-get install ros-kinetic-rosserial
 
-cd $ROBAIR_HOME/catkin_ws/src
-git clone https://github.com/ros-drivers/rosserial.git
-cd $ROBAIR_HOME/catkin_ws
-catkin_make
-catkin_make install
-source $ROBAIR_HOME/catkin_ws/devel/setup.bash
 
+
+read -r -p "Installation rosserial ? [O/n] " response
+case $response in
+	[nN]) 
+	;;
+	*)
+
+	cd $ROBAIR_HOME/catkin_ws/src
+	git clone https://github.com/ros-drivers/rosserial.git
+	cd $ROBAIR_HOME/catkin_ws
+	catkin_make
+	catkin_make install
+	source $ROBAIR_HOME/catkin_ws/devel/setup.bash
+
+	;;
+esac
 
 echo "$(tput setaf 1)Installation $(tput setab 7) Arduino$(tput sgr0)"
 
@@ -96,6 +126,8 @@ sudo apt-get install arduino
 
 
 echo "$(tput setaf 1)Arduino va être lancer veuillez accepter de rajouté dialout en rentrant votre mot de passe, puis fermer la fenêtre de Arduino$(tput sgr0)"
+
+arduino
 
 sed -i -e 's#\(.*sketchbook.path=\).*#\1'"$ROBAIR_HOME/arduino"'#' ~/.arduino/preferences.txt
 
