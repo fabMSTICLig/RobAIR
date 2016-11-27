bc9e06ac702ced7450c5af3d41281b6b71b4db97
Germain Lemasson <germain.lemasson@gmail.com>
2016-11-27 09:45:22 +0100

    correction de la résoltion de ROBAIR_IP en cas de connection offline

--- scripts/env.bash
+++ scripts/env.bash
@@ -1,4 +1,7 @@
-export ROBAIR_IP=`ip route get 8.8.8.8 | awk 'NR==1 {print $NF}'`
+export ROBAIR_IP=`ifconfig | grep 192.168 | awk -F'[: ]+' '{print $4}'`
+if [[ -z $ROBAIR_IP ]]; then
+	export ROBAIR_IP=localhost
+fi
 export PATH="$PATH:$ROBAIR_HOME/scripts/"
 source /opt/ros/kinetic/setup.bash
 source $ROBAIR_HOME/catkin_ws/devel/setup.bash
