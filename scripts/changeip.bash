275ff999ff7a758d408485bedb40f9b7f782eb18
Germain Lemasson <germain.lemasson@gmail.com>
2016-11-29 22:53:41 +0100

    ajout d'un fichier config interface par default

--- scripts/changeip.bash
+++ scripts/changeip.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
-if [ ! -z "$1" ]; then 
+if [ ! -z "$1" ]; then
 	export ROBAIR_IP=$1
 fi
 
 $ROBAIR_HOME/scripts/createDeviceCRT.bash
+if [[ ! -f  $ROBAIR_HOME/interface/public/common/js/config.js ]]; then
+	cp $ROBAIR_HOME/interface/public/common/js/config.js.default $ROBAIR_HOME/interface/public/common/js/config.js
+fi
 sed -i -e 's#\(.*serverurl : \).*#\1"'"$ROBAIR_IP"'",#' $ROBAIR_HOME/interface/public/common/js/config.js
 sed -i -e 's/\r$//g' $ROBAIR_HOME/interface/public/common/js/config.js
