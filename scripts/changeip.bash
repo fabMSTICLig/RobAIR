b185c989badeb4f13a9c97707b80a2da6c27bd8a
Germain Lemasson <germain.lemasson@gmail.com>
2016-12-08 18:36:09 +0100

    ajout du changement de bookmark chromium

--- scripts/changeip.bash
+++ scripts/changeip.bash
@@ -15,3 +15,4 @@ if [[ ! -f  $ROBAIR_HOME/interface/public/common/js/config.js ]]; then
 fi
 sed -i -e 's#\(.*serverurl : \).*#\1"'"$ROBAIR_IP"'",#' $ROBAIR_HOME/interface/public/common/js/config.js
 sed -i -e 's/\r$//g' $ROBAIR_HOME/interface/public/common/js/config.js
+sed -i -e 's#\(.*"url":\).*#\1"https://'"$ROBAIR_IP"':6080/"#' ~/.config/chromium/Default/Bookmarks
