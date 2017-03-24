e605cafd48aa6a442d5e5c034a59040cbf437eb0
Bastien Scher <bastien.scher@grenoble-inp.org>
2017-03-24 15:00:32 +0100

    Utilise des chemins relatifs pour la configuration du serveur interface
    Cela nous permet de nous débarasser du script changehome.sh, et en règle
    générale de toute dépendance au répertoire d'installation

--- scripts/changehome.bash
+++
@@ -1,7 +0,0 @@
-
-if [ ! -f $ROBAIR_HOME/interface/config.json ]; then
-  cp $ROBAIR_HOME/interface/config.json.default $ROBAIR_HOME/interface/config.json 
-fi
-sed -i -e 's#\(.*\"key\":\).*#\1"'"$ROBAIR_HOME"'/ssl/device.key",#' $ROBAIR_HOME/interface/config.json
-sed -i -e 's#\(.*\"crt\":\).*#\1"'"$ROBAIR_HOME"'/ssl/device.crt",#' $ROBAIR_HOME/interface/config.json
-sed -i -e 's#\(.*\"ca\":\).*#\1"'"$ROBAIR_HOME"'/ssl/rootCA.crt"#' $ROBAIR_HOME/interface/config.json
