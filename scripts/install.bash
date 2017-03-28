ae40e75d0cbf66a2a2b2394f48ec22361114986a
Bastien Scher <bastien.scher@grenoble-inp.org>
2017-03-28 11:16:06 +0200

    Simplifie le script d'installation

--- scripts/install.bash
+++ scripts/install.bash
@@ -30,7 +30,6 @@ if [ -z $http_proxy ]; then
 	read -r -p "Veuillez entrer votre proxy ou tapez entrée si vous n'en avez pas:" response
 	if [ ! -z "$response" ]; then
 		export http_proxy=$response
-		git config --global http.proxy $http_proxy
 	fi
 fi
 
@@ -44,16 +43,18 @@ fi
 
 echo "Veuillez entrer votre mot de passe pour installer les packages (sudo)"
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo -E apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo -E apt-get update
 
 
-echo "$(tput setaf 1)Installation $(tput setab 7)coturn nodejs npm $(tput sgr0)"
-sudo -E apt-get install coturn nodejs-legacy npm chromium-browser
+echo "$(tput setaf 1)Installation $(tput setab 7)coturn nodejs ros-kinetic arduino$(tput sgr0)"
+sudo -E apt-get install coturn nodejs-legacy npm chromium-browser \
+	ros-kinetic-ros-base ros-kinetic-rosbridge-suite arduino
 
-cd $ROBAIR_HOME/interface
-npm install
-
-cd $ROBAIR_HOME
+source /opt/ros/kinetic/setup.bash
+sudo -E rosdep init
+rosdep update
 
 read -r -p "Voulez-vous générer une autorité de certification ? [O/n] " response
 case $response in
@@ -72,45 +73,22 @@ if [[ $REPLY  =~ ^[Oo]$ ||  $REPLY =~ ^$ ]]; then
 fi
 
 
-read -r -p "Installation ros kinetic ? [O/n] "
-if [[ $REPLY  =~ ^[Oo]$ ||  $REPLY =~ ^$ ]]; then
-
-	echo "$(tput setaf 1)Installation $(tput setab 7)ros kinetic$(tput sgr0)"
-
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo -E apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
-	sudo -E apt-get update
-	sudo -E apt-get install ros-kinetic-ros-base
-	source /opt/ros/kinetic/setup.bash
-	sudo -E rosdep init
-	rosdep update
-	sudo -E apt-get install ros-kinetic-rosbridge-suite
-	source /opt/ros/kinetic/setup.bash
-
-fi
-
 # Récupère les sous-modules
 git submodule update --init
 
 # Compile les packages ROS
 (cd "$ROBAIR_HOME/catkin_ws" && catkin_make install)
-source $ROBAIR_HOME/catkin_ws/devel/setup.bash
+source "$ROBAIR_HOME/catkin_ws/devel/setup.bash"
+
+# Récupère les dépendances pour l'interface web
+(cd $ROBAIR_HOME/interface && npm install)
 
 # Configure signalmaster
-(cd $ROBAIR_HOME/signalmaster && npm install)
+(cd "$ROBAIR_HOME/signalmaster" && npm install)
 
-echo "$(tput setaf 1)Installation $(tput setab 7)Arduino$(tput sgr0)"
-echo "Veuillez vous rendre à l'adresse https://www.arduino.cc/en/Main/Software et télécharger la dernière version de arduino"
-echo "Une fois téléchargé veuillez modifier l'emplacement du carnet des croquis dans Fichier->Préférences par $ROBAIR_HOME/arduino"
-read -r -p "Veuillez appuyer sur ENTRER une fois l'installation effectuée."
-read -r -p "Voulez vous générer la librairie ros_lib pour arduino ? [O/n] "
-if [[ $REPLY  =~ ^[Oo]$ ||  $REPLY =~ ^$ ]]; then
-	echo "$(tput setaf 1)Genère $ROBAIR_HOME/arduino/libraries/ros_lib$(tput sgr0)"
-	mkdir -p $ROBAIR_HOME/arduino/libraries
-	cd $ROBAIR_HOME/arduino/libraries
-	rm -rf ros_lib
-	rosrun rosserial_arduino make_libraries.py .
-fi
-
+# Génère la ros_lib
+echo "$(tput setaf 1)Genère $ROBAIR_HOME/arduino/libraries/ros_lib$(tput sgr0)"
+rm -rf "$ROBAIR_HOME/arduino/libraries/ros_lib"
+rosrun rosserial_arduino make_libraries.py "$ROBAIR_HOME/arduino/libraries"
 
 echo "$(tput setaf 1)Installation terminée$(tput sgr0)"
