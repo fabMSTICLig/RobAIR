b1a012574e1b21a5a8aebd7b09b6daeb10377aa6
Bastien Scher <bastien.scher@grenoble-inp.org>
2017-08-28 11:30:58 +0200

    Ajoute une option pour débugguer le programme Arduino

--- simulator/main.c
+++ simulator/main.c
@@ -1,27 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "simavr/sim_avr.h"
 #include "simavr/sim_elf.h"
 #include "simavr/sim_irq.h"
 #include "simavr/sim_io.h"
+#include "simavr/sim_gdb.h"
 #include "simavr/avr_ioport.h"
 #include "simavr/parts/uart_pty.h"
 
+void print_usage(char *argv0)
+{
+	fprintf(stderr, "Usage: %s [-g] path/to/robairarduino.elf\n", argv0);
+}
+
 int main(int argc, char **argv)
 {
+	int debug = 0;
+	char *elf_path;
+
 	if (argc < 2) {
-		fprintf(stderr, "Usage: %s path/to/robairarduino.elf\n", argv[0]);
+		print_usage(argv[0]);
 		return 1;
 	}
 
+	if (strcmp(argv[1], "-g") == 0)
+		debug = 1;
+
+	if (debug && argc < 3) {
+		print_usage(argv[0]);
+		return 1;
+	}
+
+	if (debug)
+		elf_path = argv[2];
+	else
+		elf_path = argv[1];
+
 	elf_firmware_t elf;
-	elf_read_firmware(argv[1], &elf);
+	elf_read_firmware(elf_path, &elf);
 
 	avr_t *avr;
 	avr = avr_make_mcu_by_name("atmega2560");
 	avr_init(avr);
 	avr_load_firmware(avr, &elf);
+
+	if (debug) {
+		avr->gdb_port = 1234;
+		avr->state = cpu_Stopped;
+		avr_gdb_init(avr);
+	}
 
 	uart_pty_t uart_pty;
 	uart_pty_init(avr, &uart_pty);
